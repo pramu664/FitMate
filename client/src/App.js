6dec049fcbcb6d65ac61d06b40d0c650a4b5128c
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-04-20 10:39:30 +0530

    set up the client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to FitMate</h1>
+            <p>A web app that helps users track their fitness goals and progress. Users can log their workouts, track their nutrition,
+            and set reminders for their fitness routines.</p>
+        </div>
+    )
+}
+
+export default App;
