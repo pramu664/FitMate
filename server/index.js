fba1aed8db850d9c02aa340fd63400309c6e8119
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-04-24 17:57:20 +0530

    hide the CONNECTION_URL in .env file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express, { application } from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -9,9 +11,23 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://FitMate:FitMate123@fitmate.urclxjw.mongodb.net/?retryWrites=true&w=majority";
+app.use((req, res, next) => {
+    console.log("Hey, I am global middleware.");
+    console.log("Path:",req["path"], "Method:",req["method"]);
+    next();
+})
+
+app.get("/", (req, res) => {
+    console.log(process.env.PORT);
+    res.json("Welcome to my server...");
+})
+
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
+
+
+    
