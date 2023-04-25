b7f22a81ea63fe657286f8399b1cd380a5777928
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-04-25 16:28:20 +0530

    fetch all workouts and display them in home page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <Link to="/">
-                    <h1>Workout Buddy</h1>
+                    <h1>FitMate</h1>
                 </Link>
             </div>
         </header>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
+
+import WorkoutDetails from '../components/WorkoutDetails';
 
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+
+        const fetchWorkouts = async () => {
+            const response = await fetch("http://localhost:5000/api/workouts");
+            const json = await response.json();
+
+            if (response.ok) {
+                setWorkouts(json);
+            }
+        }
+
+
+        fetchWorkouts();
+    }, [])
 
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails workout={workout} key={workout._id}/>
+                ))}
+            </div>
         </div>
     )
 }
