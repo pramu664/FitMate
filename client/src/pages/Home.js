71261873c19048e15606a96e34974003b447d52a
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-04-25 19:04:18 +0530

    Design create workout form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null);
@@ -27,6 +28,7 @@ const Home = () => {
                     <WorkoutDetails workout={workout} key={workout._id}/>
                 ))}
             </div>
+            <WorkoutForm />
         </div>
     )
 }
